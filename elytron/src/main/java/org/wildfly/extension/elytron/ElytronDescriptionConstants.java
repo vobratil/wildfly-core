d40b9cbb5d20c8fca96eb5fb362b054a81fb3645
baranowb <baranowb@gmail.com>
2019-05-17 08:08:11 +0200

    [WFCORE-4314] - verbose and recursive parameter in read-alias(es)

--- elytron/src/main/java/org/wildfly/extension/elytron/ElytronDescriptionConstants.java
+++ elytron/src/main/java/org/wildfly/extension/elytron/ElytronDescriptionConstants.java
@@ -422,6 +422,7 @@ interface ElytronDescriptionConstants {
     String REALM_NAME = "realm-name";
     String REALMS = "realms";
     String REASON = "reason";
+    String RECURSIVE = "recursive";
     String REFERENCE = "reference";
     String REFERRAL_MODE = "referral-mode";
     String REGISTER_JASPI_FACTORY = "register-jaspi-factory";
@@ -560,6 +561,7 @@ interface ElytronDescriptionConstants {
     String VALIDATE = "validate";
     String VALIDITY = "validity";
     String VALUE = "value";
+    String VERBOSE = "verbose";
     String VERIFIABLE = "verifiable";
     String VERSION = "version";
     String VERSION_COMPARISON = "version-comparison";
